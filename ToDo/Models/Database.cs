04d781e885808c105d6ab4986f04c67137122daa
Scott Fraser <scottafraser@yahoo.com>
2018-07-10 12:36:50 -0700

    write tests for todo

---
+++ ToDo/Models/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using ToDo;
+
+namespace ToDo.Models
+{
+
+    public class DB
+    {
+        public static MySqlConnection Connection()
+        {
+            MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+            return conn;
+        }
+    }
+}
---
+++ ToDo/Models/Item.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDo;
+using System;
+
+namespace ToDo.Models
+{
+    public class Item
+    {
+        private int _id;
+        private string _description;
+
+        public Item(string Description, int Id = 0)
+        {
+            _id = Id;
+            _description = Description;
+        }
+
+    
+        public override bool Equals(System.Object otherItem)
+        {
+            if (!(otherItem is Item))
+            {
+                return false;
+            }
+            else
+            {
+                Item newItem = (Item)otherItem;
+                bool idEquality = (this.GetId() == newItem.GetId());
+                bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
+                return (idEquality && descriptionEquality);
+            }
+        }
+
+        public void SetId(int Id)
+        {
+            _id = Id;
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public void SetDescription(string description)
+        {
+            _description = description;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+
+        public static List<Item> GetAll()
+        {
+            List<Item> allItems = new List<Item> { };
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM items;";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int itemId = rdr.GetInt32(0);
+                string itemDescription = rdr.GetString(1);
+                Item newItem = new Item(itemDescription, itemId);
+                allItems.Add(newItem);
+            }
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return allItems;
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM items;";
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+       
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO `items` (`description`) VALUES (@ItemDescription);";
+
+            MySqlParameter description = new MySqlParameter();
+            description.ParameterName = "@ItemDescription";
+            description.Value = this._description;
+            cmd.Parameters.Add(description);
+
+            cmd.ExecuteNonQuery(); 
+            _id = (int) cmd.LastInsertedId; 
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+  }
+}
